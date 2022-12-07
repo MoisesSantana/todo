9f6f2db5c33d68b8d6e7312784b72a176cdea653
Moises Santana <moisaant@gmail.com>
2022-12-07 20:25:33 -0300

    fix and update Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,8 @@ import Logo from '../../assets/Logo.svg';
 
 export function Header() {
   return (
-    <header className="w-screen h-52 bg-neutral-900 flex justify-center align-center">
-      <img className="w-20" src={Logo} alt="logo" />
+    <header className="w-screen h-52 bg-stone-900 flex justify-center items-center">
+      <img src={Logo} alt="logo" />
     </header>
   );
 }
